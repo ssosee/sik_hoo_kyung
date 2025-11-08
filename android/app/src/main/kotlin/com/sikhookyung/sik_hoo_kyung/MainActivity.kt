c7eeffaf4020a3da558df9a9cf2b3bc3ecbad41a
howisitgoing <howisitgoing@kakao.com>
2025-11-08 22:49:41 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/sikhookyung/sik_hoo_kyung/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sikhookyung.sik_hoo_kyung
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:sik_hoo_kyung/screen/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomeScreen(),
+  ));
+}
